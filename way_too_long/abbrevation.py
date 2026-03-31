4d1a00222a24f83ba83510109ca2d4bcf2653315
fedhawaq <fedhawaaqhayiluu@gmail.com>
2026-03-31 23:04:01 +0300

    Update abbrevation.py

--- way_too_long/abbrevation.py
+++ way_too_long/abbrevation.py
@@ -1,1 +1,11 @@
+n = int(input())
+words = [input().strip() for i in range(n)]
+for word in words:
+    if len(word) > 10:
+        abbreviation = word[0] + str(len(word) - 2) + word[-1]
+        print(abbreviation)
+    else:
+        print(word)
+     
+
 
